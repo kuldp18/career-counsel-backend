2abc336ffc6e153ff0b430225432031089d38fac
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2023-02-03 18:28:40 +0530

    create Student schema

---
+++ models/student.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const { v4: uuidv4 } = require('uuid');
+
+const studentSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    encrypted_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
+
+    appointments: {
+      type: Array,
+      default: [],
+    },
+
+    sscMarks: {
+      type: Number,
+      required: false,
+    },
+    hscMarks: {
+      type: Number,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+studentSchema
+  .virtual('password')
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv4();
+    this.encrypted_password = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+studentSchema.methods = {
+  authenticate: function (plainpassword) {
+    return this.securePassword(plainpassword) === this.encrypted_password;
+  },
+
+  securePassword: function (plainpassword) {
+    if (!plainpassword) return '';
+    try {
+      return crypto
+        .createHmac('sha256', this.salt)
+        .update(plainpassword)
+        .digest('hex');
+    } catch (err) {
+      return '';
+    }
+  },
+};
+module.exports = mongoose.model('Student', studentSchema);
